eb56dd8d541cfa5af54614ac868e9ca290026e3d
guanjl1 <guanjl1@lenovo.com>
2022-01-11 10:21:02 +0800

    update

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
         '/api': {
-          target: 'http://localhost:9527', //你要访问的服务器域  https://pvtshpv1.lenovo.com.cn 
+          target: 'http://localhost:8081', //你要访问的服务器域  https://pvtshpv1.lenovo.com.cn 
           changeOrigin: true, //允许跨域
           secure: false,
           pathRewrite: {
@@ -51,7 +51,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -31,7 +31,7 @@ export default {
   created() {},
   mounted() {
 
-    this.$axios.get("/api/a", {
+    this.$axios.get("/api/admin", {
         // params: {
         //   app_type:2
         // }
